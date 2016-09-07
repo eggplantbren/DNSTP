ebcdb8be13d06316dc865e949c4a6e71f140e0d7
Brendon J. Brewer <brendon.brewer@gmail.com>
2016-09-07 16:36:33 +1200

    Setup and run sampler

--- code/main.cpp
+++ code/main.cpp
@@ -5,11 +5,29 @@
 
 int main(int argc, char** argv)
 {
-    // Create the DNSTP sampler
-    DNSTP::Sampler<DNSTP::MyModel> sampler;
+    DNest4::CommandLineOptions options(argc, argv);
 
+    std::cout<<"# Using "<<options.get_num_threads()<<" thread"<<
+    ((options.get_num_threads() == 1)?("."):("s."))<<std::endl;
+
+    std::cout<<"# Target compression factor between levels = ";
+    std::cout<<options.get_compression()<<std::endl;
+
+    // Load sampler options from file
+    DNest4::Options sampler_options(options.get_options_file().c_str());
+
+    // Create sampler
+    DNSTP::Sampler<DNSTP::MyModel> sampler(options.get_num_threads(),
+                                options.get_compression_double(),
+                                sampler_options,
+                                true);
+
+    // Seed RNGs
+    sampler.initialise(options.get_seed_uint());
+
+    // Run the sampler
     sampler.run();
 
-	return 0;
+    return 0;
 }
 
